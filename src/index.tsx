89bd996b5dd59a20896a55a5f441597e0abf2ae5
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-09-27 19:33:09 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-type IfProps = {
+export type IfProps = {
   _: boolean;
-  _then: JSX.Element;
-  _else?: JSX.Element;
-}
-export const If = ({_,_then,_else}: IfProps) => (_ ? _then : _else || null);
+  _then: JSX.Element | React.FC<any>;
+  _else?: JSX.Element | React.FC<any>;
+};
+export const If = ({_, _then, _else}: IfProps) => (_ ? (typeof _then === 'function' ? _then({}) : _then) : (typeof _else === 'function' ? _else({}) : _else) || null);
