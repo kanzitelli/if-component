bb8346e1c842616b2a83cd1a3f2f4aa42b47a971
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-10-27 20:09:53 +0200

    types fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
-type IfComponent = JSX.Element | React.FC<any>;
+export type IfComponent = JSX.Element | React.FC<any>;
 export type IfProps = {
   _: boolean;
   _then: IfComponent;
   _else?: IfComponent;
 };
-export const If = ({_, _then, _else}: IfProps): IfComponent | null =>
+export const If = ({_, _then, _else}: IfProps): JSX.Element | null =>
   _
     ? typeof _then === 'function'
       ? _then({})
       : _then
-    : (typeof _else === 'function' ? _else({}) : _else) || null;
+    : typeof _else === 'function'
+    ? _else({})
+    : _else ?? null;
