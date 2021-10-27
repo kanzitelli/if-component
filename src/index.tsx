ab7dcd120608873e83d3ed45986c7b0bddadf798
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-10-27 17:25:35 +0200

    code improvs

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+type IfComponent = JSX.Element | React.FC<any>;
 export type IfProps = {
   _: boolean;
-  _then: JSX.Element | React.FC<any>;
-  _else?: JSX.Element | React.FC<any>;
+  _then: IfComponent;
+  _else?: IfComponent;
 };
-export const If = ({_, _then, _else}: IfProps) => (_ ? (typeof _then === 'function' ? _then({}) : _then) : (typeof _else === 'function' ? _else({}) : _else) || null);
+export const If = ({_, _then, _else}: IfProps): IfComponent | null =>
+  _
+    ? typeof _then === 'function'
+      ? _then({})
+      : _then
+    : (typeof _else === 'function' ? _else({}) : _else) || null;
